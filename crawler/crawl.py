60b67e59db15293e1a182040e268e3367ab9e8d4
Geoyeob Kim <gasbank@msn.com>
2018-03-29 17:59:56 +0900

    excel

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -7,23 +7,24 @@ import string
 
 # HTTP GET Request
 for page_index in string.ascii_lowercase[1:]:
-	base_url = 'http://www.containership-info.com/'
-	req = requests.get(base_url + 'page_names_%s.html' % page_index)
-	html = req.text
-	soup = BeautifulSoup(html, 'html.parser')
-	vessels = soup.select('td > a')
-	with io.open("vessels_%s.txt" % page_index, "a", encoding='utf8') as myfile:
-		for v in vessels:
-			try:
-				v_href = v.get('href')
-				req = requests.get(base_url + v_href)
-				html = req.text
-				soup = BeautifulSoup(html, 'html.parser')
-				details = soup.select('td > table')
-				print(v_href)
-				lines = [line.strip() for line in details[0].getText().split('\n') if line.strip() != '']
-				#print("\n".join(lines))
-				myfile.write("\n".join(lines) + "\n")
-			except:
-				print("Error")
-				pass
+    base_url = 'http://www.containership-info.com/'
+    req = requests.get(base_url + 'page_names_%s.html' % page_index)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    vessels = soup.select('td > a')
+    with io.open("vessels_%s.txt" % page_index, "a", encoding='utf8') as myfile:
+        for v in vessels:
+            try:
+                v_href = v.get('href')
+                req = requests.get(base_url + v_href)
+                html = req.text
+                soup = BeautifulSoup(html, 'html.parser')
+                details = soup.select('td > table')
+                print(v_href)
+                lines = [line.strip() for line in details[0].getText().split('\n') if line.strip() != '']
+                # print("\n".join(lines))
+                myfile.write("\n".join(lines) + "\n")
+            except IOError as e:
+                print("IOError", e)
+            except e:
+                print(e)
